0ddb5c1d6e4269129914105ec1e1103e2db2f612
kabir <makkobir79@gmail.com>
2022-06-16 10:00:33 +0600

    tailwind added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,6 +8,7 @@ export default function Home() {
         <meta name="description" content="mak store.com e-commerce store" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className="bg-red-500 text-xl">Welcome to mak store</h1>
     </div>
   );
 }
